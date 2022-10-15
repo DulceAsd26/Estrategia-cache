06e8dd9365f18b794871ac94c3065325c5afaec2
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-14 23:31:57 -0500

    Correcciones

--- sw.js
+++ sw.js
@@ -4,19 +4,19 @@
 self.addEventListener('fetch', event => {
 
     const offLineResp = new Response(`
+
         Bienvenido a mi Pagina Web
         
         Disculpa pero para usarla, necesitas internet
         
         `);
 
+
+
     const resp = fetch(event.request)
-                    .catch( () => {
-
-                        return offLineResp;
-                    });
+                    .catch( () => offLineResp);
 
 
-    event.respondWith(fetch(event.request) );
+    event.respondWith( resp );
 
 });
